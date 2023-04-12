8e533b6cfffc3cb44fea281ab60d2b6912fbcf82
Rastrelly <rastrelly@gmail.com>
2023-04-12 22:21:06 +0300

    updated with chart render and small numeric scale under chart

--- cpp_sahder_text_print/cpp_sahder_text_print.cpp
+++ cpp_sahder_text_print/cpp_sahder_text_print.cpp
@@ -1,4 +1,5 @@
 ﻿#include "ourGraphics.h"
+#include <ctime>
 
 
 struct pt2 { float x, y; };
@@ -6,22 +7,62 @@ struct pt2 { float x, y; };
 Shader * shad_font; Shader * shad_chart;
 unsigned int fontTexture;
 int winx, winy;
+float mxy, mny;
+float vertScale = 1.0f, horScale = 1.0f;
 
-vector<pt2>chartdata = {};
+vector<pt2> chartdata = {};
 
-float func(float a, float b, float c, float x)
+float func(float a, float b, float c, float x, int ftype)
 {
+	if (ftype == 0)
 	return a * pow(x, 2) + b * x + c;
+	if (ftype == 1)
+	return a * sin(b*x*3.14f/180.0f + c);
+	if (ftype == 2)
+	return a * pow(x, 3) + b * pow(x,2) + c * x;
+	if (ftype == 3)
+	return a * tan(b*x*3.14f / 180.0f + c);
 }
 
-void prepcahrt(float xmin, float xstep, float xmax, vector<pt2> &cdata)
+void prepChart(float xmin, float xstep, float xmax, vector<pt2> &cdata, float &miny, float &maxy, int ft)
 {
+	std::cout << "Building chart dataset..." << std::endl;
 	cdata.clear();
 	float cx = xmin;
+	float cy = func(10, 3, -5, cx, ft);
+	float ymin = cy;
+	float ymax = cy;
 	while (cx <= xmax)
 	{
-		cdata.push_back({ cx,func(10,3,-5,cx)});
+		cy = func(10, 3, -5, cx, ft);
+		cdata.push_back({cx,cy});
+		if (cy < ymin) ymin = cy;
+		if (cy > ymax) ymax = cy;
+		cx += xstep;
 	}
+
+	miny = ymin;
+	maxy = ymax;
+}
+
+float scaleVal(float val, float scale)
+{
+	return val * scale;
+}
+
+flarr pointArrToFlArr(vector<pt2> cdata, float xscale, float yscale)
+{
+	flarr dat = {};
+	for (int i = 0; i < cdata.size(); i++)
+	{
+		dat.push_back(scaleVal(cdata[i].x,xscale));
+		dat.push_back(scaleVal(cdata[i].y,yscale));
+		dat.push_back(0.0f);
+		dat.push_back(1.0f);
+		dat.push_back(0.0f);
+		dat.push_back(0.0f);
+	}
+	return dat;
 }
 
 void prepShaders()
@@ -44,8 +85,17 @@ void processInput(GLFWwindow *window)
 
 int main()
 {
+
+	srand(time(NULL));
+
+	int ftu = rand() % 4;
+
+	float ch_start_x = -100, ch_end_x = 100;
+
 	OGLManager ogl(800, 600, framebuffer_size_callback);
 	prepShaders();
+	prepChart(ch_start_x,1, ch_end_x,chartdata,mny,mxy,ftu);
+
 	fontTexture = makeTexture("font_map.png");
 
 	while (!glfwWindowShouldClose(ogl.window))
@@ -54,21 +104,22 @@ int main()
 
 		glfwGetWindowSize(ogl.window, &winx, &winy);
 
-		glm::mat4 projection = glm::ortho((float)(-winx / 2), (float)(winx / 2),
-			(float)(-winy / 2), (float)(winy / 2), -10.0f, 10.0f);
+		glm::mat4 projection = glm::ortho(
+			(float)(-winx / 2), (float)(winx / 2),
+			(float)(-winy / 2), (float)(winy / 2), 
+			-10.0f, 10.0f);
 		glm::mat4 view = glm::mat4(1.0f);
 
 		glm::mat4 model = glm::mat4(1.0f);
 
 		float modscale = 1.0f;
-		if (winx > winy) modscale = (float)winy / 2;
-		else modscale = (float)winx / 2;
 
 		model = glm::scale(model, glm::vec3(modscale));
 
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//setting up font shader
 		shad_font->use();
 
 		shad_font->setInt("fontTexture", 0);
@@ -79,18 +130,37 @@ int main()
 
 		shad_font->setBool("useColour", false);
 
+		//setting up chart shader
 		shad_chart->use();
 
-		shad_chart->setInt("fontTexture", 0);
+		model = glm::mat4(1.0f);
 
 		shad_chart->setMatrix4f("projection", projection);
 		shad_chart->setMatrix4f("view", view);
 		shad_chart->setMatrix4f("model", model);
 
-		shad_chart->setBool("useColour", false);
+		horScale = 0.9*(winx / (ch_end_x - ch_start_x));
+		vertScale = 0.4*(winy/(mxy - mny));
 
+		//draw hor axis
+		
+
+		//draw chart line
+		drawOurVBO(pointArrToFlArr(chartdata,horScale,vertScale),6,GL_LINE_STRIP,3);
+
+		//print header
 		shad_font->use();
-		printBitmapText(-1.0f,0.9f,0.1f,"Chart output",fontTexture);
+		printBitmapText(-(winx/2.0f)+20.0f,(winy/2.0f)-40.0f,30.0f,"Chart output",fontTexture);
+
+		//print axis marks
+		float chart_scales_stp = (ch_end_x - ch_start_x) / 10.0f;
+		for (int i = 0; i < 10; i++)
+		{
+			float cPX = -(winx / 2.0f)+((float)i*(winx/10.0f));
+			char str_val[128];
+			sprintf_s(str_val, "%.1f", ch_start_x + chart_scales_stp * (float)i);
+			printBitmapText(cPX, 30.0f, 25.0f, str_val, fontTexture);
+		}
 
 		ogl.endDraw();
 	}
